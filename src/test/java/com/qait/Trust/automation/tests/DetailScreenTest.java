4c67090cdfb371ea4d1c28eea8b02cc0c0ab5fcd
Saxena, Aditya <AdityaSaxena@qainfotech.com>
2016-12-20 18:18:17 +0530

    updated detailsScreen test class

--- src/test/java/com/qait/Trust/automation/tests/DetailScreenTest.java
+++ src/test/java/com/qait/Trust/automation/tests/DetailScreenTest.java
@@ -5,63 +5,63 @@ import org.testng.annotations.Test;
 
 public class DetailScreenTest extends TestFundamentals {
 
-//    @Test
-//    public void TRUST_317_Check_For_Breadcrumb() {
-//        test.detailScreenPage.verifyBreadcrumb();
-//    }
-//
-//    @Test
-//    public void TRUST_318_Check_For_Drop_Down_Options_Available_In_Detail_Screen() {
-//        test.detailScreenPage.verifyDropDownOptionsWithOptions("last 12 hours");
-//        test.detailScreenPage.verifyDropDownOptionsWithOptions("last 24 hours");
-//        test.detailScreenPage.verifyDropDownOptionsWithOptions("last 30 days");
-//    }
-//
-//    @Test
-//    public void TRUST_319_Check_For_4_Time_Zones_Available_For_User() {
-//        test.detailScreenPage.verifyTimeZoneForUser("EST (local)");
-////        test.detailScreenPage.verifyTimeZoneForUser("IST");
-//        test.detailScreenPage.verifyTimeZoneForUser("CST");
-//        test.detailScreenPage.verifyTimeZoneForUser("PST");
-//        test.detailScreenPage.verifyTimeZoneForUser("GMT");
-//    }
-//
-//    @Test
-//    public void TRUST_321_Check_Information_Available_For_Last12_Hours() {
-//        test.detailScreenPage.verifyInformationAvailableForHours("last 12 hours", "12");
-//
-//    }
-//
-//    @Test
-//    public void TRUST_322_Check_Information_Available_For_Last24_Hours() {
-//        test.detailScreenPage.verifyInformationAvailableForHours("last 24 hours", "24");
-//
-//    }
-//
-//    @Test
-//    public void TRUST_323_Check_Information_Available_For_Last30_Days() {
-//        test.detailScreenPage.verifyInformationAvailableForLast30Days("last 30 days");
-//
-//    }
-//
-//    @Test
-//    public void Trust_324_Check_Current_Information_Alert_Button_Vailable_For_Last30Days_View() {
-//        test.detailScreenPage.checkCurrecntInformationAlertButtonforLastDays("last 30 days");
-//    }
-//
-//    @Test
-//    public void Trust_340_Check_Current_Information_Alert_Button_Vailable_For_Last12Hours_View() {
-//        test.detailScreenPage.checkCurrecntInformationAlertButtonforLastDays("last 12 hours");
-//    }
-//
-//    @Test
-//    public void Trust_345_Check_Current_Information_Alert_Button_Vailable_For_Last24Hours_View() {
-//        test.detailScreenPage.checkCurrecntInformationAlertButtonforLastDays("last 24 hours");
-//    }
+    @Test
+    public void TRUST_317_Check_For_Breadcrumb() {
+        test.detailScreenPage.verifyBreadcrumb();
+    }
 
     @Test
-    public void Trust_346_Check_For_Color_Notation_Green_In_The_Platform_Availability_And_Detail_Screen() {
-        test.detailScreenPage.checkColorNotationGreenInPlatformAvailability();
-        test.detailScreenPage.checkColorNotationGreenInDetailScreen();
+    public void TRUST_318_Check_For_Drop_Down_Options_Available_In_Detail_Screen() {
+        test.detailScreenPage.verifyDropDownOptionsWithOptions("last 12 hours");
+        test.detailScreenPage.verifyDropDownOptionsWithOptions("last 24 hours");
+        test.detailScreenPage.verifyDropDownOptionsWithOptions("last 30 days");
     }
+
+    @Test
+    public void TRUST_319_Check_For_4_Time_Zones_Available_For_User() {
+        test.detailScreenPage.verifyTimeZoneForUser("EST (local)");
+//        test.detailScreenPage.verifyTimeZoneForUser("IST");
+        test.detailScreenPage.verifyTimeZoneForUser("CST");
+        test.detailScreenPage.verifyTimeZoneForUser("PST");
+        test.detailScreenPage.verifyTimeZoneForUser("GMT");
+    }
+
+    @Test
+    public void TRUST_321_Check_Information_Available_For_Last12_Hours() {
+        test.detailScreenPage.verifyInformationAvailableForHours("last 12 hours", "12");
+
+    }
+
+    @Test
+    public void TRUST_322_Check_Information_Available_For_Last24_Hours() {
+        test.detailScreenPage.verifyInformationAvailableForHours("last 24 hours", "24");
+
+    }
+
+    @Test
+    public void TRUST_323_Check_Information_Available_For_Last30_Days() {
+        test.detailScreenPage.verifyInformationAvailableForLast30Days("last 30 days");
+
+    }
+
+    @Test
+    public void Trust_324_Check_Current_Information_Alert_Button_Vailable_For_Last30Days_View() {
+        test.detailScreenPage.checkCurrecntInformationAlertButtonforLastDays("last 30 days");
+    }
+
+    @Test
+    public void Trust_340_Check_Current_Information_Alert_Button_Vailable_For_Last12Hours_View() {
+        test.detailScreenPage.checkCurrecntInformationAlertButtonforLastDays("last 12 hours");
+    }
+
+    @Test
+    public void Trust_345_Check_Current_Information_Alert_Button_Vailable_For_Last24Hours_View() {
+        test.detailScreenPage.checkCurrecntInformationAlertButtonforLastDays("last 24 hours");
+    }
+
+//    @Test
+//    public void Trust_346_Check_For_Color_Notation_Green_In_The_Platform_Availability_And_Detail_Screen() {
+//        test.detailScreenPage.checkColorNotationGreenInPlatformAvailability();
+//        test.detailScreenPage.checkColorNotationGreenInDetailScreen();
+//    }
 }
