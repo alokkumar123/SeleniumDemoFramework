ad348e5d9a9eb90871bfc4d2f5f1e52c3417b138
Sehgal, Harsh <HarshSehgal@qainfotech.com>
2016-12-27 13:25:53 +0530

    Removed unneccessary console and unused variables from keywords

--- src/test/java/com/qait/Trust/automation/tests/DetailScreenTest.java
+++ src/test/java/com/qait/Trust/automation/tests/DetailScreenTest.java
@@ -4,23 +4,19 @@ import com.qait.Trust.automation.TestFundamentals;
 import org.testng.annotations.Test;
 
 public class DetailScreenTest extends TestFundamentals {
-
-//    @Test
-//    public void verifyPlatfromAvailabilityPattern() {
-//        test.detailScreenPage.verifyPlatfromAvalabilityPattern();
-//    }
     
     @Test
     public void TRUST_317_Check_For_Breadcrumb() {
         test.detailScreenPage.verifyBreadcrumb();
     }
-
+    
     @Test
     public void TRUST_318_Check_For_Drop_Down_Options_Available_In_Detail_Screen() {
         test.detailScreenPage.verifyDropDownOptionsWithOptions("last 12 hours");
         test.detailScreenPage.verifyDropDownOptionsWithOptions("last 24 hours");
         test.detailScreenPage.verifyDropDownOptionsWithOptions("last 30 days");
     }
+    
     @Test
     public void TRUST_319_Check_For_4_Time_Zones_Available_For_User() {
         test.detailScreenPage.verifyTimeZoneForUser("EST (local)");
