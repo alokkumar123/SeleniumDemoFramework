619aef1d32a6a64ce0a365e48fb3692f6f062fbe
Saxena, Aditya <AdityaSaxena@qainfotech.com>
2017-01-12 16:14:57 +0530

    Automated TRUST-395

--- src/test/java/com/qait/Trust/automation/keywords/CheckSearchFunctionalityCyclePageActions.java
+++ src/test/java/com/qait/Trust/automation/keywords/CheckSearchFunctionalityCyclePageActions.java
@@ -17,6 +17,8 @@ public class CheckSearchFunctionalityCyclePageActions extends GetPage {
     }
 
     String searchingText;
+    String description = "Created new system by automation script";
+    String newSystem = "New System";
 
     public void validateSearchFunctionalityForPossitiveCycle() {
         searchingText = "M";
@@ -58,5 +60,43 @@ public class CheckSearchFunctionalityCyclePageActions extends GetPage {
         element("input_serachBox").sendKeys(searchingText);
         ReportMsg.info("Validated no result is availble after performing " + searchingText + " in search box");
 
+    }
+
+    public void userCheckCreateNewSystem() {
+        isElementDisplayed("button_Create");
+        element("button_Create").click();
+        isElementDisplayed("h4_createSystem");
+        ReportMsg.info(" User navigated to Create System screen");
+    }
+
+    public void userCreateNewSystemFormAndSave() {
+
+        isElementDisplayed("div_SystemName");
+        element("div_SystemName").click();
+        element("div_SystemName").clear();
+        element("div_SystemName").sendKeys(newSystem);
+        ReportMsg.info("Entered system name : " + newSystem + " in mandatory field");
+        element("div_discription").click();
+        element("div_discription").clear();
+        element("div_discription").sendKeys(description);
+        ReportMsg.info("Entered description name : " + description + " in mandatory field");
+        isElementDisplayed("button_save");
+        element("button_save").click();
+        ReportMsg.info("User Created new System Form and Saved Form");
+    }
+
+    public void savedDataShouldBeSeenInTheListScreen() {
+        isElementDisplayed("link_newCreatedSystem", newSystem);
+        ReportMsg.info("Verified new system created  by automatrion script named as " + newSystem + " is displaying on Screen list");
+        isElementDisplayed("text_description", description);
+        ReportMsg.info("Verified description of created system named as  " + description + " is displaying on on Screen list");
+    }
+
+    public void verifyMoveUpFunctionality() {
+        String appBeforeMoveUp = element("text_moveUp").getText();
+        ReportMsg.info("App on 3rd row before clicking on moveUp " + appBeforeMoveUp);
+        element("link_moveUp").click();
+        String appAfterMoveUp = element("text_moveUp").getText();
+        ReportMsg.info("App on 3rd row after clicking on moveUp " + appAfterMoveUp);
     }
 }
---
+++ src/test/java/com/qait/Trust/automation/tests/ConfiguredSystemTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qait.Trust.automation.tests;
+
+import com.qait.Trust.automation.TestFundamentals;
+import org.testng.annotations.Test;
+
+public class ConfiguredSystemTest extends TestFundamentals {
+
+    @Test
+    public void TC001_Configure_System() {
+        test.createAndConfigPage.logAsAdmin();
+        test.createAndConfigPage.navigateToAdminMenu("Manage systems");
+        test.createAndConfigPage.gridShouldBeAvailableFor("Enabled");
+        test.createAndConfigPage.gridShouldBeAvailableFor("Monitors");
+        test.createAndConfigPage.gridShouldBeAvailableFor("Tab order");
+        test.createAndConfigPage.gridShouldBeAvailableFor("System");
+        test.createAndConfigPage.gridShouldBeAvailableFor("Description");
+        test.createAndConfigPage.createButtonAndSearchTextBarShouldBeAvailable();
+        test.createAndConfigPage.breadcrumbShouldBeAvailableForNavigation("Admin");
+    }
+
+    @Test 
+    public void TC002_Check_Move_Up_Functionality(){
+        test.checkSearchFunctionality.verifyMoveUpFunctionality();
+    }
+    
+    @Test
+    public void TC003_Check_Search_Functionality_Positive_Cycle() {
+        test.createAndConfigPage.navigateToAdminMenu("Manage systems");
+        test.checkSearchFunctionality.validateSearchFunctionalityForPossitiveCycle();
+    }
+    
+    @Test
+    public void TC004_Check_Search_Functionality_Negative_Cycle() {
+        test.checkSearchFunctionality.validateSearchFunctionalityForNegativeCycle();
+    }
+
+    @Test
+    public void TC005_Check_And_Create_New_System() {
+        test.checkSearchFunctionality.userCheckCreateNewSystem();
+        test.checkSearchFunctionality.userCreateNewSystemFormAndSave();
+        test.checkSearchFunctionality.savedDataShouldBeSeenInTheListScreen();
+    }
+}
