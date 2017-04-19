3dcc68eaa2e35d487c459b53f6fadc8ff3dbb111
Saxena, Aditya <AdityaSaxena@qainfotech.com>
2017-04-19 15:53:15 +0530

    Automated TRUST-598

---
+++ src/test/java/com/qait/Trust/automation/tests/ModifyNotificationAndVerifyThatItUpdatesToTheUnauthenticatedUserTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qait.Trust.automation.tests;
+
+import com.qait.Trust.automation.TestFundamentals;
+import static com.qait.Trust.automation.utils.ConfigPropertyReader.getProperty;
+import static com.qait.Trust.automation.utils.YamlReader.getData;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author alokkumar
+ */
+public class ModifyNotificationAndVerifyThatItUpdatesToTheUnauthenticatedUserTest extends TestFundamentals {
+
+    String userName = getProperty("userName");
+    String password = getProperty("password");
+
+    String monitorName = null;
+    String message = "test of notification for Modified Notification";
+    String updatedMessage = "Updated test of notification for Modified Notification";
+    String systemView;
+    String eventType1 = "Planned Maintenance";
+    String eventType2 = "Informational Message";
+
+    @Test
+    public void TRUST_598_TC01_Login_With_Admin_User() {
+        systemView = test.platformAvailabilityPage.verifyViewModeOnSplashPage();
+        monitorName = test.platformAvailabilityPage.getMonitor(getData("appNameforRegion.4ltr"), systemView);
+        test.createAndConfigPage.logAsAdmin(userName, password);
+    }
+
+    @Test
+    public void TRUST_598_TC02_Create_Notification_For_Single_System() {
+        test.createNotificationPage.clickOnNotificationButton();
+        test.createNotificationPage.selectEventType(eventType1);
+        test.createNotificationPage.selectMonitorfromDropDown(monitorName);
+        test.createNotificationPage.setEndDateForMessageInformation();
+        test.createNotificationPage.enterValueInCommentSection(message);
+        test.createNotificationPage.clickOnSaveButton();
+        test.detailScreenPage.userNavigateToPlatfromAvailableScreenWhenClickOnPlatformAvailabilityHome();
+        test.createAndConfigPage.logOutFromAdmin();
+
+    }
+
+    @Test
+    public void TRUST_598_TC03_Verify_Created_Notification_Displays_To_Unauthenticated_User() {
+        String splashPageView = test.platformAvailabilityPage.verifyMonitroIsDisplayingOnSplashPage(getData("appNameforRegion.4ltr"), systemView);
+        test.createNotificationPage.verifyMessageAndLogoIsDisplayingOrNotDisplayingToUnauthenticatedUser(splashPageView, message, eventType1);
+
+    }
+
+    @Test
+    public void TRUST_598_TC04_Modify_Notification_And_Verify_Modified_Notification_Displays_To_Unauthenticated_User() {
+        test.createAndConfigPage.logAsAdmin(userName, password);
+        test.createNotificationPage.searchCreatedNotification(message);
+        test.createNotificationPage.clickOnCreatedNotificationMessage(message, monitorName);
+        test.createNotificationPage.selectEventType(eventType2);
+        test.createNotificationPage.enterValueInCommentSection(updatedMessage);
+        test.createNotificationPage.clickOnSaveButton();
+        test.detailScreenPage.userNavigateToPlatfromAvailableScreenWhenClickOnPlatformAvailabilityHome();
+        test.createAndConfigPage.logOutFromAdmin();
+        String splashPageView = test.platformAvailabilityPage.verifyMonitroIsDisplayingOnSplashPage(getData("appNameforRegion.4ltr"), systemView);
+        test.createNotificationPage.verifyMessageAndLogoIsDisplayingOrNotDisplayingToUnauthenticatedUser(splashPageView, updatedMessage, eventType2);
+    }
+
+    @Test
+    public void TRUST_598_TC05_Delete_Created_Notifaction_By_Automation_Script() {
+        test.createAndConfigPage.logAsAdmin(userName, password);
+        test.createNotificationPage.searchCreatedNotification(updatedMessage);
+        test.createNotificationPage.deleteCreatedNotification(updatedMessage);
+        test.createAndConfigPage.logOutFromAdmin();
+    }
+}
